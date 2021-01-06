e1389df7e0530a12f5f0a1f3197fda7ab3b46657
BenSa0112 <benny-sachs@gmx.de>
2021-01-06 12:20:02 +0100

    1.1

--- organisator/settings.py
+++ organisator/settings.py
@@ -19,14 +19,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'exhlfdat&vfum(-34*c2uroi(($ww(yo$9pv98=e6p^gl(-eoj'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['organisator.jesus-lebt.com', '127.0.0.1']
-
 
 # Application definition
 
